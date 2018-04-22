4c05fac9e9d2fa5d25e532de3431a759732ec912
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-22 09:35:22 +0300

    add Tile script

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FifteenPuzzle
+{
+    public class Tile : MonoBehaviour
+    {
+        public Text Text;
+
+        public Canvas canvas;
+
+        private int number;
+
+        private float GetSize()
+        {
+            var rectTransform = canvas.GetComponent<RectTransform>();
+            return rectTransform.rect.width * rectTransform.localScale.x;
+        }
+
+        public void Init(int newNumber)
+        {
+            Number = newNumber;
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                Text.text = number.ToString();
+            }
+        }
+    
+        public void SetSize(float newSize)
+        {
+            var scaleMult = newSize / GetSize();
+            transform.localScale = Vector3.one * scaleMult;
+        }
+    }
+}
